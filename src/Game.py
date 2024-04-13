8572a66beba7901612159d9349a69e59f74af4dc
KarolyJ <jonaskaroly15@gmail.com>
2024-04-13 19:24:06 +0200

    progress with the textfield

--- src/Game.py
+++ src/Game.py
@@ -28,6 +28,9 @@ data = file.readlines()
 # reload rate for the words
 word_reload = 20
 
+# input rectangle
+input_rect = pygame.Rect(300, 450, 140, 32)
+
 clock = pygame.time.Clock()
 FPS = 30
 
@@ -40,29 +43,54 @@ def main():
     sprites = pygame.sprite.Group()
     word_reload_counter = word_reload
 
+    user_text = ""
+
     while running:
         # if word_reload_counter is not zero then subtract one
         # so go through the refreshes *word_reload* times then create a sprite
         if word_reload_counter:
             word_reload_counter -= 1
         else:
-            word = Word(data[random.randint(0, len(data)-1)], GREEN, 5)
+            word = Word(data[random.randint(0, len(data)-1)].strip(), GREEN, 5, user_text)
+            print(word.inputText)
             sprites.add(word)
             word_reload_counter = word_reload
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+
+                # check for backspace
+                if event.key == pygame.K_BACKSPACE:
+
+                    # get text input from 0 to -1 i.e end.
+                    user_text = user_text[:-1]
+
+                # Unicode standard is used for string
+                # formation
+                else:
+                    user_text += event.unicode
+
+        text_surface = my_font.render(user_text, True, WHITE)
+        # set width of textfield so that text cannot get
+        # outside of user's text input
+        input_rect.w = max(100, text_surface.get_width() + 10)
 
         # Update and draw sprites only if there are events
         sprites.update()
         screen.blit(bg_image, (0, 0))  # Draw background image
         sprites.draw(screen)  # Draw words
-        pygame.display.update()
+
+        # draw the input rectangle
+        pygame.draw.rect(screen, GREEN, input_rect)
+        # render at position stated in arguments
+        screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
+
+        pygame.display.flip()
         clock.tick(FPS)
 
 
 # The main function
 if __name__ == "__main__":
-    print(data[0])
     main()
     pygame.quit()
--- src/Word.py
+++ src/Word.py
@@ -10,12 +10,13 @@ my_font = pygame.font.SysFont("arialunicode", 20)
 # Defining word entities in the game
 # Making it inherit the Sprite class
 class Word(pygame.sprite.Sprite):
-    def __init__(self, value, color, speed):
+    def __init__(self, value, color, speed, inputText):
         # Inheriting the sprite class
         pygame.sprite.Sprite.__init__(self)
         self.value = value
         self.color = color
         self.speed = speed
+        self.inputText = inputText
         # Must needed info for the draw method to work
         self.image = my_font.render(self.value, True, self.color)
         self.rect = self.image.get_rect()
@@ -29,6 +30,8 @@ class Word(pygame.sprite.Sprite):
     def update(self):
         self.rect.y += self.speed * self.speed / 10
         if self.rect.y > 500:
+            self.kill()
+        elif self.inputText == self.value:
             self.kill()
 
     def remove(self, words):
